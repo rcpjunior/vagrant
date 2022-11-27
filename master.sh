65686ee0966ee3c32daa12347b5356254e286178
ROBERTO CARLOS PEREIRA JUNIOR <63765065+rcpjunior@users.noreply.github.com>
2022-11-27 13:53:59 -0300

    1.2
    Implementação com Virtualbox

--- master.sh
+++ master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker swarm init --advertise-addr=172.23.0.100
-sudo docker swarm join-token worker | grep docker > worker.sh
+sudo docker swarm init --advertise-addr=192.168.56.100
+sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
---
+++ worker.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-2ya8jvvoxxgtsej2kk8yvkpcoxct22daqoprirav0tgeqkcmc6-64jx96so43jq7nc7jc4263w5n 192.168.56.100:2377
